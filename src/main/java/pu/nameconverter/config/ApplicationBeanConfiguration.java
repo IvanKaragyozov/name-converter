7c669fb4e005b7b45972c5ca0d083238ee9c3ce1
Ivan Karagyozov <ivankaragyozov0@gmail.com>
2023-03-23 15:48:00 +0200

    Removed unnecessary modelmapper

--- src/main/java/pu/nameconverter/config/ApplicationBeanConfiguration.java
+++ src/main/java/pu/nameconverter/config/ApplicationBeanConfiguration.java
@@ -1,16 +1,10 @@
 package pu.nameconverter.config;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationBeanConfiguration {
-
-    @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
 
     @Bean
     public StringBuilder stringBuilder(){
